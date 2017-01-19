3a761f6ec8c85368ed1613e1a841fc9e70bd340b
Trav Easton <travzdevil69@hotmail.com>
2017-01-20 03:02:31 +0800

    Check if tmux is installed and add attach alias

--- basherk.sh
+++ basherk.sh
@@ -177,7 +177,9 @@ alias travmysql='mysql -u trav -p'
 alias mysql_shutdown='mysqladmin -u trav -p shutdown'
 alias elmd='now && tailf /var/log/mysqld.log'
 alias ipas='ip addr show | hlp "inet .*/"'
-
+if exists tmux; then
+    alias attach='tmux attach'
+fi
 
 # Git aliases
 alias gb='git branch -a'
