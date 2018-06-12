4e5b327e1d374f92979457bcad3d4a8c1e3a62d6
Trav Easton <travzdevil69@hotmail.com>
2018-06-12 19:28:39 +0800

    Remove github API token from source control 🙃

--- pre-basherk.sh
+++ pre-basherk.sh
@@ -1,7 +1,5 @@
 # pre-basherk
 # © Trav Easton 2016
 
-export HOMEBREW_GITHUB_API_TOKEN="a7b95e6f3d32667199ed99e753345d0c7c4af33a"
-
 # fix for mail before anything else
 [[ $HOSTNAME != *"mail"* ]] && shopt -s nocasematch
