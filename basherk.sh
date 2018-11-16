ecf668ca0a1246676462026ed9e100623fad81a8
Trav Easton <travzdevil69@hotmail.com>
2018-11-16 20:03:54 +0800

    Make osascript in tab 2-char indentation

--- basherk.sh
+++ basherk.sh
@@ -730,16 +730,16 @@ function strpos() {
         fi
 
         osascript &>/dev/null <<EOF
-            tell application "iTerm"
-                tell current window
-                    set newTab to (create tab with default profile)
-                    tell newTab
-                        tell current session
-                            write text "cd \"$cdto\"$cmd"
-                        end tell
-                    end tell
+          tell application "iTerm"
+            tell current window
+              set newTab to (create tab with default profile)
+              tell newTab
+                tell current session
+                  write text "cd \"$cdto\"$cmd"
                 end tell
+              end tell
             end tell
+          end tell
 EOF
     }
 }
