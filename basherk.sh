traveaston/basherk

commit 0b2347ed68ae3b3df5fc632320aab9be77691a96

Remove searchcontents, use only as backup for f

basherk.sh
@@ -268,7 +268,6 @@ function f() {
     local location=$1
     local search=$2
     local sudo=$3
-    local args
     local hl=${search/\*/}
     hl=${hl/./\\.}
 
@@ -295,21 +294,18 @@ function f() {
         $sudo find ${PATH//:/ } -maxdepth 1 -name "$search" -print | hlp "$hl"
     } elif [[ $location == "in" ]]; then {
         # search file contents
-        if [[ "$3" != "--old" ]]; then {
-            args="$@"
-            args=${args:3}
+        local args=$(echo "$@" | perl -pe 's/^in //')
 
-            if exists rg; then {
-                rg $args
-                return
-            } elif exists ag; then {
-                ag $args
-                return
-            } else {
-                searchcontents $args
-            } fi
+        # prefer ripgrep, then silver surfer, then grep if neither are installed
+        if exists rg; then {
+            rg "$args"
+        } elif exists ag; then {
+            ag "$args"
         } else {
-            searchcontents "$2" $4 $5
+            echo "searching $(pwf) for '$search' (case insensitive, ignoring binary files, .git/, and vendor/)"
+            count=$(grep --color=always -Iinr "$search" . --exclude-dir=".git" --exclude-dir="vendor" | tee /dev/tty | wc -l)
+
+            echo "$count matches"
         } fi
     } elif [[ $location == "commit" ]]; then {
         # find a commit with message matching string
@@ -491,25 +487,6 @@ function scanip() {
 
 function scansubnet() {
     scan_nmap 192.168.$1.1 $2
-}
-
-function searchcontents() {
-    if [[ "$1" != "" ]]; then
-        echo "searching $(pwf) for '$1' (ignoring binary files, .git/, vendor/)"
-        grepString="grep -"
-        [[ "$2" != "0" ]] && grepString+="i"
-        grepString+="Inr \"$1\" . --exclude-dir=\".git\" --exclude-dir=\"vendor\" --exclude-dir"
-        [[ "$3" != "0" ]] && grepString+=" --exclude-dir=\"js\""
-
-        eval $grepString
-        count=$(eval $grepString | wc -l)
-
-        echo "$count matches"
-    else
-        echo "usage: searchcontents string [bool caseInsensitive?] [bool excludeJS?]"
-        echo "       searchcontents 'string with spaces' [1|0] [1|0]"
-        echo "       searchcontents '\$pecial' [1|0] [1|0]"
-    fi
 }
 
 function set_title() {

commit ac7711b2cdab2caf8b1f2d7480e96ada028f46a2

Remove inoperable args parsing from f
When searching file contents with f, we detect which of rg/ag/grep to
use, in that order.
Originally all other arguments passed to f would be passed through to
the relevant tool. `f in test -i` would map to `grep "test" -i ...`

Due to incorrect quoting, this functionality regressed in 0b2347e.

Furthermore, since the fallback to grep is meant for compatibility on
barebones servers without rg/ag installed, assuming perl is installed
(for the args regex) is a mistake, and it regularly isn't.

basherk.sh
@@ -439,14 +439,13 @@ function f() {
         $sudo find ${PATH//:/ } -maxdepth 1 -name "$search" -print | hlp "$hl"
     elif [[ $location == "in" ]]; then
         # search file contents
-        local args=$(echo "$@" | perl -pe 's/^in //')
 
         if [[ $tool != "grep" ]]; then
             echo "searching ${CYAN}$(pwf)${D} for '${CYAN}$search${D}' (using $tool)"
-            $tool "$args"
+            $tool "$search"
         else
             echo "searching ${CYAN}$(pwf)${D} for '${CYAN}$search${D}' (using $tool, ignores: case, binaries , .git/, vendor/)"
-            count=$($tool --color=always -Iinr "$args" . --exclude-dir=".git" --exclude-dir="vendor" | tee /dev/tty | wc -l)
+            count=$($tool --color=always -Iinr "$search" . --exclude-dir=".git" --exclude-dir="vendor" | tee /dev/tty | wc -l)
 
             echo "$count matches"
         fi