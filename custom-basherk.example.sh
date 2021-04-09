9b31b07bf8a977f2ebdf98812900dbb200224ad2
Trav Easton <travzdevil69@hotmail.com>
2021-04-09 19:13:52 +0800

    Add custom-basherk example file

---
+++ custom-basherk.example.sh
@@ -0,0 +1,13 @@
+# custom user aliases & functions
+# sourced by basherk directly after pre-basherk.sh (i.e. before most other code)
+# usage: copy this example file to expected filename and modify
+# cp custom-basherk.example.sh custom-basherk.sh
+
+
+# export MY_VAR="foo"
+
+# alias my_alias='my_func'
+
+# my_func() {
+#     …
+# }
